1182e20ce16acd9ea2450704ab3e015440c714b5
Gabriel <knuckles4x@gmail.com>
2021-05-20 05:06:02 -0300

    pagination

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Posts } from "../../components/Posts";
+import { Button } from "../../components/Button";
 import { loadPost } from "../../utils/load-post";
 
 import "./styles.css";
@@ -7,6 +8,9 @@ import "./styles.css";
 class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
   };
 
   async componentDidMount() {
@@ -14,16 +18,37 @@ class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPost();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
         <Posts posts={posts} />
+        <div className="button-container">
+          <Button
+            text="Ver mais"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
       </section>
     );
   }
