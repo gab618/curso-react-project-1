ac93c3e93c51b36005bf1aab88ef87a26bc25993
Gabriel <knuckles4x@gmail.com>
2021-07-08 00:19:19 -0300

    Button tests

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Button } from ".";
 
 describe("<Button />", () => {
@@ -9,5 +10,29 @@ describe("<Button />", () => {
 
     const button = screen.getByRole("button", { name: /load more/i });
     expect(button).toBeInTheDocument();
+  });
+
+  it("should call function on button click", () => {
+    const fn = jest.fn();
+    render(<Button text="load more" onClick={fn} />);
+
+    const button = screen.getByRole("button", { name: /load more/i });
+    userEvent.click(button);
+    // fireEvent.click(button);
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should be disabled when disabled is true", () => {
+    render(<Button text="load more" disabled={true} />);
+
+    const button = screen.getByRole("button", { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
+
+  it("should be enabled when disabled is false", () => {
+    render(<Button text="load more" disabled={false} />);
+
+    const button = screen.getByRole("button", { name: /load more/i });
+    expect(button).toBeEnabled();
   });
 });
