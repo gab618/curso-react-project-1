8030ed37eabc88ce0a89324805ffd3b1ea102711
Gabriel <knuckles4x@gmail.com>
2021-07-19 17:15:15 -0300

    PostCard test: render

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { PostCard } from ".";
+import { postCardPropsMock } from "./mock";
+
+describe("<PostCard />", () => {
+  it("should render PostCard correctly", () => {
+    render(<PostCard post={postCardPropsMock} />);
+
+    expect(
+      screen.getByRole("img", { name: postCardPropsMock.title })
+    ).toHaveAttribute("src", postCardPropsMock.cover);
+
+    expect(
+      screen.getByRole("heading", { name: postCardPropsMock.title })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText(postCardPropsMock.body)).toBeInTheDocument();
+  });
+});
---
+++ src/components/PostCard/mock.js
@@ -0,0 +1,6 @@
+export const postCardPropsMock = {
+  id: 1,
+  body: "body 1",
+  title: "Meu título massa",
+  cover: "https://github.com/gab618.png",
+};
